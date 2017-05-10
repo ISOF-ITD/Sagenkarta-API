57cda37d398e7750350af12e50da0e91814c5266
traustid <traustid@gmail.com>
2017-05-10 11:27:57 +0200

    Create config.demo.php

---
+++ config.demo.php
@@ -0,0 +1,12 @@
+<?php
+
+// here we make errors visible for debugging purpose
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+$dbhost = "";
+$dbuser = "";
+$dbpass = "";
+$dbname = "svenska_sagor";
+
+?>
